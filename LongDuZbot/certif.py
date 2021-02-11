695539761b6a261a324826b9e00c7d87cf4a72c7
guigur <guigur13@gmail.com>
2021-02-11 05:04:19 +0100

    card 4 money

--- LongDuZbot/certif.py
+++ LongDuZbot/certif.py
@@ -1,30 +1,36 @@
 from PIL import Image, ImageOps, ImageDraw, ImageFont
 from datetime import datetime
 import locale
+import random
+
+secure_random = random.SystemRandom()
 
 locale.setlocale(locale.LC_ALL, 'fr_FR') #For the french date
 
 textColor = (0,0,0,255)
 
+def genRoundImg(source):
+	bigsize = (source.size[0] * 3, source.size[1] * 3)
+	mask = Image.new('L', bigsize, 0)
+	draw = ImageDraw.Draw(mask) 
+	draw.ellipse((0, 0) + bigsize, fill=255)
+	mask = mask.resize(source.size, Image.ANTIALIAS)
+	source.putalpha(mask)
+
 def generateCertifMaster(profilePictureLink, pseudo, score):
 	profile = Image.open(profilePictureLink) #profileg profile
 	profile = profile.resize([512, 512]) #just to be sure
 
-	certif = Image.open('img/certif.png', 'r')
-	tampon = Image.open('img/academie.png', 'r')
+	certif = Image.open('img/template/certif_best.png', 'r')
+	tampon = Image.open('img/academie_best.png', 'r')
 	signature = Image.open('img/signature.png', 'r')
 
 	profile_w, profile_h  = profile.size
 	profileg_w, profileg_h = certif.size
 	tampon_w, tampon_h = tampon.size
 	signature_w, signature_h = signature.size
-
-	bigsize = (profile.size[0] * 3, profile.size[1] * 3)
-	mask = Image.new('L', bigsize, 0)
-	draw = ImageDraw.Draw(mask) 
-	draw.ellipse((0, 0) + bigsize, fill=255)
-	mask = mask.resize(profile.size, Image.ANTIALIAS)
-	profile.putalpha(mask)
+	
+	genRoundImg(profile)
 
 	profile = ImageOps.fit(profile, mask.size, centering=(0.5, 0.5))
 	profile.putalpha(mask)
@@ -54,14 +60,14 @@ def generateCertifMaster(profilePictureLink, pseudo, score):
 	certif.paste(tampon, (1700, 750, 1700 + tampon_w, 750 + tampon_h), tampon)
 	certif.paste(signature, (1400, 750, 1400 + signature_w, 750 + signature_h), signature)
 
-	return certif.save("tmp/certif_filled.png")
+	return certif.save("tmp/certif_best_filled.png")
 
 def generateCertifBitch(profilePictureLink, pseudo, score):
 	profile = Image.open(profilePictureLink) #profileg profile
 	profile = profile.resize([512, 512]) #just to be sure
 
-	certif = Image.open('img/certif_pute.png', 'r')
-	tampon = Image.open('img/academie_pute.png', 'r')
+	certif = Image.open('img/template/certif_worst.png', 'r')
+	tampon = Image.open('img/academie_worst.png', 'r')
 	signature = Image.open('img/signature.png', 'r')
 
 	profile_w, profile_h  = profile.size
@@ -69,15 +75,7 @@ def generateCertifBitch(profilePictureLink, pseudo, score):
 	tampon_w, tampon_h = tampon.size
 	signature_w, signature_h = signature.size
 
-	bigsize = (profile.size[0] * 3, profile.size[1] * 3)
-	mask = Image.new('L', bigsize, 0)
-	draw = ImageDraw.Draw(mask) 
-	draw.ellipse((0, 0) + bigsize, fill=255)
-	mask = mask.resize(profile.size, Image.ANTIALIAS)
-	profile.putalpha(mask)
-
-	profile = ImageOps.fit(profile, mask.size, centering=(0.5, 0.5))
-	profile.putalpha(mask)
+	genRoundImg(profile)
 
 	certif.paste(profile, (768, 364, 768 + profile_w, 364 + profile_h), profile)
 
@@ -108,4 +106,51 @@ def generateCertifBitch(profilePictureLink, pseudo, score):
 	certif.paste(tampon, (1700, 750, 1700 + tampon_w, 750 + tampon_h), tampon)
 	certif.paste(signature, (1400, 750, 1400 + signature_w, 750 + signature_h), signature)
 
-	return certif.save("tmp/certif_pute_filled.png")
+	return certif.save("tmp/certif_worst_filled.png")
+
+
+
+def generateMoneyCard(profilePictureLink, serverPictureLink, pseudo, money):
+	ownSynonyms = ["Possède", "Cache au Fisc", "A", "Détient", "Dispose", "Garde", "Conserve"]
+	bankSynonyms = ["en banque.", "dans son porte-feuilles.", "dans son porte-monnaie."]
+	profile = Image.open(profilePictureLink)
+	profile = profile.resize([128, 128])
+
+	server = Image.open(serverPictureLink)
+	server = server.resize([48, 48])
+
+	fontCardBig = ImageFont.truetype(r'font/Helvetica.ttf', 48) 
+	fontCard = ImageFont.truetype(r'font/Helvetica.ttf', 24) 
+	fontCardAccent = ImageFont.truetype(r'font/Helvetica-Bold.ttf', 32) 
+
+	card = Image.open('img/template/card.png', 'r')
+	
+	genRoundImg(profile)
+	card.paste(profile, (20, 20, 20 + profile.size[0], 20 + profile.size[1]), profile)
+	genRoundImg(server)
+	card.paste(server, (100, 100, 100 + server.size[0], 100 + server.size[1]), server)
+	ligneOffset = [20, 85, 110]
+
+	draw = ImageDraw.Draw(card)
+	draw.text((188, ligneOffset[0]), text=pseudo, fill=(255,255,255,255), font=fontCardBig, anchor=None, spacing=0, align="left")
+
+	textOwn = secure_random.choice(ownSynonyms) + " "
+	textOwnW,textOwnH = fontCard.getsize(textOwn)
+	draw.text((188, ligneOffset[1]), text=textOwn, fill=(255,255,255,255), font=fontCard, anchor=None, spacing=0, align="left")
+
+	textNumber = str(money)
+	textNumberW,textNumberH = fontCardAccent.getsize(textNumber)
+	draw.text((188 + textOwnW, ligneOffset[1] - 5), text=textNumber, fill=(255,255,255,255), font=fontCardAccent, anchor=None, spacing=0, align="left")
+
+	textMoneyName = " WADs"
+	textMoneyNameW,textMoneyNameH = fontCard.getsize(textMoneyName)
+	draw.text((188 + textOwnW + textNumberW, ligneOffset[1]), text=textMoneyName, fill=(255,255,255,255), font=fontCard, anchor=None, spacing=0, align="left")
+
+	textInBank = secure_random.choice(bankSynonyms)
+	textInBankW,textInBankH = fontCard.getsize(textInBank)
+	draw.text((188, ligneOffset[2]), text=textInBank, fill=(255,255,255,255), font=fontCard, anchor=None, spacing=0, align="left")
+
+	#card.show()
+	return card.save("tmp/card_filled.png")
+
+#generateMoneyCard("img/debug/avatar.png", "img/debug/server.png", "guigur", 200)
