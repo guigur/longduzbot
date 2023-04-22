68556d1c71e0a82d11424740bbe2b78f9bc58217
guigur <guigur13@gmail.com>
2023-04-22 23:19:09 +0200

    cleanup emotes

--- LongDuZbot/ggr_emotes.py
+++ LongDuZbot/ggr_emotes.py
@@ -1,92 +1,29 @@
-flingued = "<:flingued:806653166606221373>" #ok
-flingueg = "<:flingueg:806653171300302858>" #ok
+flingued = "<:flingued:806653166606221373>"
+flingueg = "<:flingueg:806653171300302858>"
 
-#salty = "<a:salty:788971656726839306>" 
+maing = "<:maing:806653170197594193>"
+maind = "<:maind:806653175511777330>"
 
-maing = "<:maing:806653170197594193>" #ok
-maind = "<:maind:806653175511777330>" #ok
-
-#lilian3flip = "<:lilian3flip:>"
-#Lilian3 = "<:Lilian3:>"
-
-DrakeOK = "<:DrakeOK:806653157689524224>" #ok
-DrakeNO = "<:DrakeNO:806653109958082591>" #ok
-
-#Knife = "<:Knife:>"
-
-#teub = "<:teub:784174377205104671>"
-
-#petou = "<:petou:>"
+DrakeOK = "<:DrakeOK:806653157689524224>"
+DrakeNO = "<:DrakeNO:806653109958082591>"
 
 Ulian = "<:Ulian:806653174642507826>"
 Moth = "<:Moth:806653171316686908>"
 Guigor = "<:Guigor:806653160482668574>"
-#SaloperiedoreeOld = "<:Saloperiedoree:788967092115603486>"
-Saloperiedoree = "<:SD:806652869150244884>"#"<a:SD:806652869150244884>" #recup l'annim sur le pc fixe
+
+Saloperiedoree = "<:SD:806652869150244884>"
+#"<a:SD:806652869150244884>" #recup l'annim sur le pc fixe
 WAD = "<:Lilian1:806653173136883764"
 
-lilinkhisrightelbow = "<:lilinkhisrightelbow:784177962970513438>"
-lilinkhisface = "<:lilinkhisface:784177963008524288>"
-lilinkhisleftelbow = "<:lilinkhisleftelbow:784177962961338378>"
-lilinkhisrightarm = "<:lilinkhisrightarm:784177962986504213>"
-lilinkhisbody = "<:lilinkhisbody:784177962865262643>"
-lilinkhisleftarm = "<:lilinkhisleftarm:784177962978246686>"
-lilinkhisrightleg = "<:lilinkhisrightleg:784182056790786059>"
-lilinkhiscrotch = "<:lilinkhiscrotch:784182057152151582>"
-lilinkhisleftleg = "<:lilinkhisleftleg:784182057093038150>"
-lilinkhisrightfoot = "<:lilinkhisrightfoot:784184399536455681>"
-lilinkhisleftfoot = "<:lilinkhisleftfoot:784184399511814186>"
-lilinkhislefttoes = "<:lilinkhislefttoes:784184399955755029>"
-
-faces = {'Tim': '<:Tim:784142816896352296>', 
-'Pretzel': '<:Pretzel:>',
-'Poscoll': '<:Poscoll:>',
-'Poscol2': '<:Poscol2:>',
-'Poscol1': '<:Poscol1:>',
-'Paul': '<:Paul:>',
-'Pat3': '<:Pat3:>',
-'Pat2': '<:Pat2:>',
-'Pat1': '<:Pat1:>',
-'Olox6': '<:Olox6:>',
-'Olox5': '<:Olox5:>',
-'Olox4': '<:Olox4:>',
-'Olox3': '<:Olox3:>',
-'Olox2': '<:Olox2:>',
-'Olox1': '<:Olox1:>',
-'Olox': '<:Olox:>',
-'Noe1': '<:Noe1:>',
-'Noe': '<:Noe:>',
-'Nico': '<:Nico:>',
-'Mat7': '<:Mat7:>',
-'Mat6': '<:Mat6:>',
-'Mat5': '<:Mat5:>',
-'Mat4': '<:Mat4:>',
-'Mat3': '<:Mat3:>',
-'Mat2': '<:Mat2:>',
-'Mat1': '<:Mat1:>',
-'Mae2': '<:Mae2:>',
-'Mae1': '<:Mae1:>',
-'Lilian7': '<:Lilian7:>',
-'Lilian6': '<:Lilian6:>',
-'Lilian5': '<:Lilian5:>',
-'Lilian4': '<:Lilian4:>',
-'Lilian2': '<:Lilian2:784148660010352670>',
-'lilian': '<:lilian:>',
-'Guillaume': '<:Guillaume:>',
-'Franck': '<:Franck:>',
-'Forian': '<:Forian:>',
-'Flo': '<:Flo:>',
-'Chriss': '<:Chriss:>',
-'Chat': '<:Chat:>',
-'Charlotte': '<:Charlotte:>',
-'Carotte': '<:Carotte:>',
-'Benoir2': '<:Benoir2:>',
-'Benoir1': '<:Benoir1:>',
-'Arnould': '<:Arnould:>',
-'Antoinos2': '<:Antoinos2:>',
-'Antoinos1': '<:Antoinos1:>',
-'Anna3': '<:Anna3:>',
-'Anna2': '<:Anna2:>',
-'Anna': '<:Anna:>',
-'AlexPetard': '<:AlexPetard:>',
-}
+# lilinkhisrightelbow = "<:lilinkhisrightelbow:784177962970513438>"
+# lilinkhisface = "<:lilinkhisface:784177963008524288>"
+# lilinkhisleftelbow = "<:lilinkhisleftelbow:784177962961338378>"
+# lilinkhisrightarm = "<:lilinkhisrightarm:784177962986504213>"
+# lilinkhisbody = "<:lilinkhisbody:784177962865262643>"
+# lilinkhisleftarm = "<:lilinkhisleftarm:784177962978246686>"
+# lilinkhisrightleg = "<:lilinkhisrightleg:784182056790786059>"
+# lilinkhiscrotch = "<:lilinkhiscrotch:784182057152151582>"
+# lilinkhisleftleg = "<:lilinkhisleftleg:784182057093038150>"
+# lilinkhisrightfoot = "<:lilinkhisrightfoot:784184399536455681>"
+# lilinkhisleftfoot = "<:lilinkhisleftfoot:784184399511814186>"
+# lilinkhislefttoes = "<:lilinkhislefttoes:784184399955755029>"
